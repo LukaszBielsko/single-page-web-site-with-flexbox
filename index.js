32f39a5fa3fe9887bdd2004af8a218b9d9b136bd
Lukasz Walczak <lukeabove@gmail.com>
2020-01-22 09:05:44 +0100

    google maps added

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ document.querySelector('#submit').addEventListener('click', (e) => {
   e.preventDefault()
   alert('thank you for your message')
 })
+
+var map;
+function initMap() {
+  var bb = { lat: 49.8224, lng: 19.0584 };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: bb,
+    zoom: 8
+  });
+  var marker = new google.maps.Marker({ position: bb, map });
+}
